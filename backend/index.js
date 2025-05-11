072707c1e75d748af92b59a74005a453bf679861
stive1213 <estifanosamsalu833@gmail.com>
2025-05-11 05:05:33 -0700

    test

--- backend/index.js
+++ backend/index.js
@@ -143,6 +143,9 @@ app.post("/api/logout", (req, res) => {
       res.json({ message: "Logged out" });
     });
   });
-});
+});  
+
+
+
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
